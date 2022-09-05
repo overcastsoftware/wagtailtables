be644482fb1ca8f36e75745971f28c4992331857
Sævar Öfjörð Magnússon <saevar@saevar.is>
2022-09-05 13:39:50 +0000

    Update version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ documentation_extras = [
 
 setup(
     name='wagtailtables',
-    version='0.1',
+    version='0.2',
     packages=['wagtailtables'],
     include_package_data=True,
     license='MIT',
